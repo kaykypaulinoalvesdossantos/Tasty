4ddf93eb0ffcb585e96a671827f33666079f70a8
kayky_paulino <98046923+kaykypaulinoalvesdossantos@users.noreply.github.com>
2022-04-17 18:08:26 -0300

    Resolução de bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,15 +54,13 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*==================== SHOW SCROLL TOP ====================*/
-function scrollHeader(){
+function scrollTop(){
     const scrollTop = document.getElementById('scroll-top')
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('scroll-top')
 }
 
 window.addEventListener('scroll', scrollTop)
-
-
 
 /*==================== DARK LIGHT THEME ====================*/
 const themeButton = document.getElementById('theme-button')
@@ -92,21 +90,4 @@ themeButton.addEventListener('click', () => {
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
-})
-
-/*==================== SCROLL REVEAL ANIMATION ====================*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 2000,
-    reset: true
-});
-
-sr.reveal(`.home__data, .home__img,
-            .about__data, .about__img,
-            .services__content, .menu__content,
-            .app__data, .app__img,
-            .contact__data, .contact__button,
-            .footer__content`, {
-    interval: 200
 })
